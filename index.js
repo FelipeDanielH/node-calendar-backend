4b02e303ca989bf849d560acc6abfd3c79d06004
Felipe Daniel <felipe.daniel.henriquez@gmail.com>
2023-03-30 16:27:51 -0300

    Agregada variable de entorno para el puerto

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require('express');
+require('dotenv').config();
 
 // Crear el Servidor de express
 const app = express();
 
+// Directorio Publico
+app.use( express.static('./public'));
+
 // Rutas
-app.get('/', (req,res) =>{
-    res.json({
-        of:true
-    })
-})
+// app.get('/', (req,res) =>{
+//     res.json({
+//         of:true
+//     })
+// })
 
 //Escuchar peticiones
-app.listen( 4000, () =>{
-    console.log(`Servidor corriendo en puerto ${4000}`)
+app.listen( process.env.PORT, () =>{
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`)
 } )
