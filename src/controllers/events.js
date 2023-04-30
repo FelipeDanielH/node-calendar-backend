9bb6baeec20cd2c380e32b1ff3de53a716013890
Felipe Daniel <felipe.daniel.henriquez@gmail.com>
2023-04-30 16:21:39 -0400

    se añade funcion para guardar eventos en la base de datos

--- src/controllers/events.js
+++ src/controllers/events.js
@@ -1,4 +1,5 @@
-const { response } = require('express')
+const { response } = require('express');
+const Evento = require('../models/Evento');
 
 const getEventos = (req, res = response) => {
     res.json( {
@@ -7,15 +8,27 @@ const getEventos = (req, res = response) => {
     });
 }
 
-const crearEvento = (req, res = response) => {
+const crearEvento = async (req, res = response) => {
 
-    // verificar que tenga el evento.
-    console.log( req.body );
+    const evento = new Evento( req.body );
 
-    res.json( {
-        ok: true,
-        msg: 'crear evento'
-    });
+    try{
+
+        evento.user = req.uid;
+        const eventoGuardado = await evento.save();
+
+        res.json({
+            ok: true,
+            evento: eventoGuardado
+        })
+
+    }catch (error){
+        console.log(error)
+        res.status(500).json({
+            ok:false,
+            msg:'Hable con el administrador'
+        })
+    }
 }
 
 const actualizarEvento = (req, res = response) => {
