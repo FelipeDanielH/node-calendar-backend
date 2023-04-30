135e15b76c7d624d7707c55267a4ba4b7172931f
Felipe Daniel <felipe.daniel.henriquez@gmail.com>
2023-04-30 14:39:32 -0400

    se agregan validadores al endpoint de agregar evento

--- src/routes/events.js
+++ src/routes/events.js
@@ -2,10 +2,13 @@
     Event Routes
     api/events
 */
-
 const Router = require('express');
+const { check } = require('express-validator');
+
+const { validarCampos } = require('../middlewares/validar-campos');
 const { validarJWT } = require('../middlewares/validar-jwt');
 const { getEventos, crearEvento, actualizarEvento, eliminarEvento} = require('../controllers/events');
+const { isDate } = require('../helpers/isDate');
 
 const router = Router();
 
@@ -13,15 +16,22 @@ const router = Router();
 router.use( validarJWT );
 
 // Obtener eventos
-router.get('/', getEventos)
+router.get('/', getEventos);
 
 // Crear un nuevo evento
-router.post('/', crearEvento)
+router.post('/', [ //Middlewares
+    check('title', 'El titulo es obligatorio').not().isEmpty(),
+    check('start', 'Fecha de inicio es obligatoria').custom(isDate),
+    check('end', 'Fecha de fin es obligatoria').custom(isDate),
+    validarCampos
+],
+crearEvento
+);
 
 // Actualizar evento
 router.put('/:id', actualizarEvento );
 
 // Eliminar evento
-router.delete('/:id', eliminarEvento)
+router.delete('/:id', eliminarEvento);
 
 module.exports = router;
